8f7e30e2b3f24f2b64e84f331ecb407af4b778bd
liujunhong <liujunhong@vispractice.com>
2022-11-18 18:38:23 +0800

    debug custom md files

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -661,7 +661,7 @@ export default defineConfigWithTheme<ThemeConfig>({
     config(md) {
       md.use(headerPlugin).use(jobsPlugin)
     },
-    sidebarDepth : 2
+    sidebarDepth : 3
     // // toc: { level: [1, 2, 3] },
     // headers: { level: [2, 3, 4] }
   },
