8289727f5e752860a77a6499646007cd09461169
liujunhong <liujunhong@vispractice.com>
2022-11-18 13:46:05 +0800

    debug custom md files

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -618,39 +618,39 @@ export default defineConfigWithTheme<ThemeConfig>({
     // Placeholder of the i18n config for @vuejs-translations.
     // i18n,
 
-    algolia: {
-      indexName: 'vuejs',
-      appId: 'ML0LEBN7FQ',
-      apiKey: 'f49cbd92a74532cc55cfbffa5e5a7d01',
-      searchParameters: {
-        facetFilters: ['version:v3']
-      }
-    },
+    // algolia: {
+    //   indexName: 'vuejs',
+    //   appId: 'ML0LEBN7FQ',
+    //   apiKey: 'f49cbd92a74532cc55cfbffa5e5a7d01',
+    //   searchParameters: {
+    //     facetFilters: ['version:v3']
+    //   }
+    // },
 
     carbonAds: {
       code: 'CEBDT27Y',
       placement: 'vuejsorg'
-    },
-
-    socialLinks: [
-      { icon: 'languages', link: '/translations/' },
-      { icon: 'github', link: 'https://github.com/vuejs/' },
-      { icon: 'twitter', link: 'https://twitter.com/vuejs' },
-      { icon: 'discord', link: 'https://discord.com/invite/HBherRA' }
-    ],
-
-    editLink: {
-      repo: 'vuejs/docs',
-      text: 'Edit this page on GitHub'
-    },
-
-    footer: {
-      license: {
-        text: 'MIT License',
-        link: 'https://opensource.org/licenses/MIT'
-      },
-      copyright: `Copyright © 2014-${new Date().getFullYear()} Evan You`
     }
+
+    // socialLinks: [
+    //   { icon: 'languages', link: '/translations/' },
+    //   { icon: 'github', link: 'https://github.com/vuejs/' },
+    //   { icon: 'twitter', link: 'https://twitter.com/vuejs' },
+    //   { icon: 'discord', link: 'https://discord.com/invite/HBherRA' }
+    // ],
+
+    // editLink: {
+    //   repo: 'vuejs/docs',
+    //   text: 'Edit this page on GitHub'
+    // },
+
+    // footer: {
+    //   license: {
+    //     text: 'MIT License',
+    //     link: 'https://opensource.org/licenses/MIT'
+    //   },
+    //   copyright: `Copyright © 2014-${new Date().getFullYear()} Evan You`
+    // }
   },
 
   markdown: {
