6905c629409c53df8abf167717d680accbd4ab08
liujunhong <liujunhong@vispractice.com>
2022-12-15 19:34:22 +0800

    update menu structure

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -455,8 +455,8 @@ export default defineConfigWithTheme<ThemeConfig>({
   themeConfig: {
     nav,
     sidebar,
-    // 修改顶部栏位的内容，在此配置
-
+    // 修改顶部栏位的内容，在此配置【.】
+    logo: '/logo.svg'
     // sidebarDepth : 3
     // Placeholder of the i18n config for @vuejs-translations.
     // i18n,
