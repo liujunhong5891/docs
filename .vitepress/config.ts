3aace2e20d58f1d6f51b063c3102bf89d4a0b38b
liujunhong5891 <13631623073@139.com>
2023-04-03 17:06:45 +0800

    add the quickstart-create section

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -119,7 +119,7 @@ export const sidebar: ThemeConfig['sidebar'] = {
     {
       text: '快速入门',
       items: [
-        { text: '从零安装部署运行时环境', link: '/guide/user-guide/quickstart-01' },
+        { text: '从零安装部署运行时环境', link: '/guide/user-guide/user-guide-00' },
         { text: '销毁部署运行时环境', link: '/guide/default' }
       ]
     },
