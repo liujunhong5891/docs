4bf8cc9d3909192a3d48a2c04520062bc566e0d9
liujunhong5891 <13631623073@139.com>
2023-04-06 11:06:42 +0800

    update menus

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -18,25 +18,25 @@ const nav: ThemeConfig['nav'] = [
       // { text: 'Quick Start', link: '/guide/quick-start' }
       // { text: 'Style Guide', link: '/style-guide/' },
     // ]
-  },
-  {
-    text: 'API',
-    activeMatch: `^/api/`,
-    link: ''
-    // link: '/api/'
-  },
-  {
-    text: 'Github',
-    link: 'https://github.com/lanbingcloud'
-  },
-  {
-    text: '视频',
-    link: 'https://space.bilibili.com/404408807/?spm_id_from=333.999.0.0'
-  },
-  {
-    text: '社区',
-    link: ''
   }
+  // {
+  //   text: 'API',
+  //   activeMatch: `^/api/`,
+  //   link: ''
+  //   // link: '/api/'
+  // },
+  // {
+  //   text: 'Github',
+  //   link: 'https://github.com/lanbingcloud'
+  // },
+  // {
+  //   text: '视频',
+  //   link: 'https://space.bilibili.com/404408807/?spm_id_from=333.999.0.0'
+  // },
+  // {
+  //   text: '社区',
+  //   link: ''
+  // }
   // {
   //   text: 'Ecosystem',
   //   activeMatch: `^/ecosystem/`,
@@ -103,31 +103,30 @@ export const sidebar: ThemeConfig['sidebar'] = {
     {
       text: '概述',
       items: [
-        { text: '概述', link: '/guide/docs-overview' },
+        { text: '概述', link: '/guide/user-guide/user-guide-00' },
       ]
     },
-    {
-      text: '核心概念',
-      items: [
-        {
-          text: 'gitops',
-          link: '/guide/default'
-        },
-        { text: 'operator', link: '/guide/default' }
-      ]
-    },
+    // {
+    //   text: '核心概念',
+    //   items: [
+    //     {
+    //       text: 'gitops',
+    //       link: '/guide/default'
+    //     },
+    //     { text: 'operator', link: '/guide/default' }
+    //   ]
+    // },
     {
       text: '快速入门',
       items: [
         { text: '自动安装 Nautes', link: '/guide/user-guide/quickstart-03' },
-        { text: '从零安装部署运行时环境', link: '/guide/user-guide/quickstart-01' },
+        { text: '从零开始安装部署运行时环境', link: '/guide/user-guide/quickstart-01' },
         { text: '销毁部署运行时环境', link: '/guide/user-guide/quickstart-02' }
       ]
     },
     {
       text: '用户指南',
       items: [
-        { text: '概述', link: '/guide/user-guide/user-guide-00' },
         { text: '注册集群', link: '/guide/default' },
         { text: '维护产品', link: '/guide/user-guide/user-guide-01' },
         { text: '维护项目', link: '/guide/user-guide/user-guide-02' },
@@ -136,271 +135,271 @@ export const sidebar: ThemeConfig['sidebar'] = {
         { text: '维护部署运行时', link: '/guide/user-guide/user-guide-05' },
         { text: '跟踪部署过程和结果', link: '/guide/user-guide/user-guide-06' }
       ]
-    },
-    {
-      text: '开发指南',
-      items: [
-        { text: '筹备中', link: '/guide/default' }
-      ]
-    },
-    {
-      text: '运维指南',
-      items: [
-        { text: '筹备中', link: '/guide/default' }
-      ]
-    },
-    {
-      text: 'API参考',
-      items: [
-        {
-          text: '筹备中',
-          link: '/guide/default'
-        }
-      ]
-    },
-    {
-      text: '常见问题',
-      items: [
-        { text: '筹备中', link: '/guide/default' }
-      ]
-    },
-    {
-      text: '演示DEMO',
-      items: [
-        {
-          text: '通过ArgoCD部署和管理CI/CD环境',
-          link: '/guide/CICD-quickStart'
-        },
-        {
-          text: '用Tekton和ArgoEvents构建Kubernetes原生的流水线',
-          link: '/guide/CICD-quickStart'
-        },
-        { text: '集成Tekton和ArgoCD', link: '/guide/default' },
-        { text: 'Github界面访问Tekton和ArgoCD', link: '/guide/default' },
-        { text: '实现Kubernetes/Tekton/ArgoCD的统一认证', link: '/guide/default' },
-        { text: '实现Kubernetes/Tekton/ArgoCD的统一权限', link: '/guide/default' }
-      ]
     }
-  ],
-  '/api/': [
-    {
-      text: 'Global API',
-      items: [
-        { text: 'Application', link: '/api/application' },
-        {
-          text: 'General',
-          link: '/api/general'
-        }
-      ]
-    },
-    {
-      text: 'Composition API',
-      items: [
-        { text: 'setup()', link: '/api/composition-api-setup' },
-        {
-          text: 'Reactivity: Core',
-          link: '/api/reactivity-core'
-        },
-        {
-          text: 'Reactivity: Utilities',
-          link: '/api/reactivity-utilities'
-        },
-        {
-          text: 'Reactivity: Advanced',
-          link: '/api/reactivity-advanced'
-        },
-        {
-          text: 'Lifecycle Hooks',
-          link: '/api/composition-api-lifecycle'
-        },
-        {
-          text: 'Dependency Injection',
-          link: '/api/composition-api-dependency-injection'
-        }
-      ]
-    },
-    {
-      text: 'Options API',
-      items: [
-        { text: 'Options: State', link: '/api/options-state' },
-        { text: 'Options: Rendering', link: '/api/options-rendering' },
-        {
-          text: 'Options: Lifecycle',
-          link: '/api/options-lifecycle'
-        },
-        {
-          text: 'Options: Composition',
-          link: '/api/options-composition'
-        },
-        { text: 'Options: Misc', link: '/api/options-misc' },
-        {
-          text: 'Component Instance',
-          link: '/api/component-instance'
-        }
-      ]
-    },
-    {
-      text: 'Built-ins',
-      items: [
-        { text: 'Directives', link: '/api/built-in-directives' },
-        { text: 'Components', link: '/api/built-in-components' },
-        {
-          text: 'Special Elements',
-          link: '/api/built-in-special-elements'
-        },
-        {
-          text: 'Special Attributes',
-          link: '/api/built-in-special-attributes'
-        }
-      ]
-    },
-    {
-      text: 'Single-File Component',
-      items: [
-        { text: 'Syntax Specification', link: '/api/sfc-spec' },
-        { text: '<script setup>', link: '/api/sfc-script-setup' },
-        { text: 'CSS Features', link: '/api/sfc-css-features' }
-      ]
-    },
-    {
-      text: 'Advanced APIs',
-      items: [
-        { text: 'Render Function', link: '/api/render-function' },
-        { text: 'Server-Side Rendering', link: '/api/ssr' },
-        { text: 'TypeScript Utility Types', link: '/api/utility-types' },
-        { text: 'Custom Renderer', link: '/api/custom-renderer' }
-      ]
-    }
-  ],
-  '/examples/': [
-    {
-      text: 'Basic',
-      items: [
-        {
-          text: 'Hello World',
-          link: '/examples/#hello-world'
-        },
-        {
-          text: 'Handling User Input',
-          link: '/examples/#handling-input'
-        },
-        {
-          text: 'Attribute Bindings',
-          link: '/examples/#attribute-bindings'
-        },
-        {
-          text: 'Conditionals and Loops',
-          link: '/examples/#conditionals-and-loops'
-        },
-        {
-          text: 'Form Bindings',
-          link: '/examples/#form-bindings'
-        },
-        {
-          text: 'Simple Component',
-          link: '/examples/#simple-component'
-        }
-      ]
-    },
-    {
-      text: 'Practical',
-      items: [
-        {
-          text: 'Markdown Editor',
-          link: '/examples/#markdown'
-        },
-        {
-          text: 'Fetching Data',
-          link: '/examples/#fetching-data'
-        },
-        {
-          text: 'Grid with Sort and Filter',
-          link: '/examples/#grid'
-        },
-        {
-          text: 'Tree View',
-          link: '/examples/#tree'
-        },
-        {
-          text: 'SVG Graph',
-          link: '/examples/#svg'
-        },
-        {
-          text: 'Modal with Transitions',
-          link: '/examples/#modal'
-        },
-        {
-          text: 'List with Transitions',
-          link: '/examples/#list-transition'
-        },
-        {
-          text: 'TodoMVC',
-          link: '/examples/#todomvc'
-        }
-      ]
-    },
-    {
-      // https://eugenkiss.github.io/7guis/
-      text: '7 GUIs',
-      items: [
-        {
-          text: 'Counter',
-          link: '/examples/#counter'
-        },
-        {
-          text: 'Temperature Converter',
-          link: '/examples/#temperature-converter'
-        },
-        {
-          text: 'Flight Booker',
-          link: '/examples/#flight-booker'
-        },
-        {
-          text: 'Timer',
-          link: '/examples/#timer'
-        },
-        {
-          text: 'CRUD',
-          link: '/examples/#crud'
-        },
-        {
-          text: 'Circle Drawer',
-          link: '/examples/#circle-drawer'
-        },
-        {
-          text: 'Cells',
-          link: '/examples/#cells'
-        }
-      ]
-    }
-  ],
-  '/style-guide/': [
-    {
-      text: 'Style Guide',
-      items: [
-        {
-          text: 'Overview',
-          link: '/style-guide/'
-        },
-        {
-          text: 'A - Essential',
-          link: '/style-guide/rules-essential'
-        },
-        {
-          text: 'B - Strongly Recommended',
-          link: '/style-guide/rules-strongly-recommended'
-        },
-        {
-          text: 'C - Recommended',
-          link: '/style-guide/rules-recommended'
-        },
-        {
-          text: 'D - Use with Caution',
-          link: '/style-guide/rules-use-with-caution'
-        }
-      ]
-    }
+    // {
+    //   text: '开发指南',
+    //   items: [
+    //     { text: '筹备中', link: '/guide/default' }
+    //   ]
+    // },
+    // {
+    //   text: '运维指南',
+    //   items: [
+    //     { text: '筹备中', link: '/guide/default' }
+    //   ]
+    // },
+    // {
+    //   text: 'API参考',
+    //   items: [
+    //     {
+    //       text: '筹备中',
+    //       link: '/guide/default'
+    //     }
+    //   ]
+    // },
+    // {
+    //   text: '常见问题',
+    //   items: [
+    //     { text: '筹备中', link: '/guide/default' }
+    //   ]
+    // },
+    // {
+    //   text: '演示DEMO',
+    //   items: [
+    //     {
+    //       text: '通过ArgoCD部署和管理CI/CD环境',
+    //       link: '/guide/CICD-quickStart'
+    //     },
+    //     {
+    //       text: '用Tekton和ArgoEvents构建Kubernetes原生的流水线',
+    //       link: '/guide/CICD-quickStart'
+    //     },
+    //     { text: '集成Tekton和ArgoCD', link: '/guide/default' },
+    //     { text: 'Github界面访问Tekton和ArgoCD', link: '/guide/default' },
+    //     { text: '实现Kubernetes/Tekton/ArgoCD的统一认证', link: '/guide/default' },
+    //     { text: '实现Kubernetes/Tekton/ArgoCD的统一权限', link: '/guide/default' }
+    //   ]
+    // }
   ]
+  // '/api/': [
+  //   {
+  //     text: 'Global API',
+  //     items: [
+  //       { text: 'Application', link: '/api/application' },
+  //       {
+  //         text: 'General',
+  //         link: '/api/general'
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     text: 'Composition API',
+  //     items: [
+  //       { text: 'setup()', link: '/api/composition-api-setup' },
+  //       {
+  //         text: 'Reactivity: Core',
+  //         link: '/api/reactivity-core'
+  //       },
+  //       {
+  //         text: 'Reactivity: Utilities',
+  //         link: '/api/reactivity-utilities'
+  //       },
+  //       {
+  //         text: 'Reactivity: Advanced',
+  //         link: '/api/reactivity-advanced'
+  //       },
+  //       {
+  //         text: 'Lifecycle Hooks',
+  //         link: '/api/composition-api-lifecycle'
+  //       },
+  //       {
+  //         text: 'Dependency Injection',
+  //         link: '/api/composition-api-dependency-injection'
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     text: 'Options API',
+  //     items: [
+  //       { text: 'Options: State', link: '/api/options-state' },
+  //       { text: 'Options: Rendering', link: '/api/options-rendering' },
+  //       {
+  //         text: 'Options: Lifecycle',
+  //         link: '/api/options-lifecycle'
+  //       },
+  //       {
+  //         text: 'Options: Composition',
+  //         link: '/api/options-composition'
+  //       },
+  //       { text: 'Options: Misc', link: '/api/options-misc' },
+  //       {
+  //         text: 'Component Instance',
+  //         link: '/api/component-instance'
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     text: 'Built-ins',
+  //     items: [
+  //       { text: 'Directives', link: '/api/built-in-directives' },
+  //       { text: 'Components', link: '/api/built-in-components' },
+  //       {
+  //         text: 'Special Elements',
+  //         link: '/api/built-in-special-elements'
+  //       },
+  //       {
+  //         text: 'Special Attributes',
+  //         link: '/api/built-in-special-attributes'
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     text: 'Single-File Component',
+  //     items: [
+  //       { text: 'Syntax Specification', link: '/api/sfc-spec' },
+  //       { text: '<script setup>', link: '/api/sfc-script-setup' },
+  //       { text: 'CSS Features', link: '/api/sfc-css-features' }
+  //     ]
+  //   },
+  //   {
+  //     text: 'Advanced APIs',
+  //     items: [
+  //       { text: 'Render Function', link: '/api/render-function' },
+  //       { text: 'Server-Side Rendering', link: '/api/ssr' },
+  //       { text: 'TypeScript Utility Types', link: '/api/utility-types' },
+  //       { text: 'Custom Renderer', link: '/api/custom-renderer' }
+  //     ]
+  //   }
+  // ],
+  // '/examples/': [
+  //   {
+  //     text: 'Basic',
+  //     items: [
+  //       {
+  //         text: 'Hello World',
+  //         link: '/examples/#hello-world'
+  //       },
+  //       {
+  //         text: 'Handling User Input',
+  //         link: '/examples/#handling-input'
+  //       },
+  //       {
+  //         text: 'Attribute Bindings',
+  //         link: '/examples/#attribute-bindings'
+  //       },
+  //       {
+  //         text: 'Conditionals and Loops',
+  //         link: '/examples/#conditionals-and-loops'
+  //       },
+  //       {
+  //         text: 'Form Bindings',
+  //         link: '/examples/#form-bindings'
+  //       },
+  //       {
+  //         text: 'Simple Component',
+  //         link: '/examples/#simple-component'
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     text: 'Practical',
+  //     items: [
+  //       {
+  //         text: 'Markdown Editor',
+  //         link: '/examples/#markdown'
+  //       },
+  //       {
+  //         text: 'Fetching Data',
+  //         link: '/examples/#fetching-data'
+  //       },
+  //       {
+  //         text: 'Grid with Sort and Filter',
+  //         link: '/examples/#grid'
+  //       },
+  //       {
+  //         text: 'Tree View',
+  //         link: '/examples/#tree'
+  //       },
+  //       {
+  //         text: 'SVG Graph',
+  //         link: '/examples/#svg'
+  //       },
+  //       {
+  //         text: 'Modal with Transitions',
+  //         link: '/examples/#modal'
+  //       },
+  //       {
+  //         text: 'List with Transitions',
+  //         link: '/examples/#list-transition'
+  //       },
+  //       {
+  //         text: 'TodoMVC',
+  //         link: '/examples/#todomvc'
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     // https://eugenkiss.github.io/7guis/
+  //     text: '7 GUIs',
+  //     items: [
+  //       {
+  //         text: 'Counter',
+  //         link: '/examples/#counter'
+  //       },
+  //       {
+  //         text: 'Temperature Converter',
+  //         link: '/examples/#temperature-converter'
+  //       },
+  //       {
+  //         text: 'Flight Booker',
+  //         link: '/examples/#flight-booker'
+  //       },
+  //       {
+  //         text: 'Timer',
+  //         link: '/examples/#timer'
+  //       },
+  //       {
+  //         text: 'CRUD',
+  //         link: '/examples/#crud'
+  //       },
+  //       {
+  //         text: 'Circle Drawer',
+  //         link: '/examples/#circle-drawer'
+  //       },
+  //       {
+  //         text: 'Cells',
+  //         link: '/examples/#cells'
+  //       }
+  //     ]
+  //   }
+  // ],
+  // '/style-guide/': [
+  //   {
+  //     text: 'Style Guide',
+  //     items: [
+  //       {
+  //         text: 'Overview',
+  //         link: '/style-guide/'
+  //       },
+  //       {
+  //         text: 'A - Essential',
+  //         link: '/style-guide/rules-essential'
+  //       },
+  //       {
+  //         text: 'B - Strongly Recommended',
+  //         link: '/style-guide/rules-strongly-recommended'
+  //       },
+  //       {
+  //         text: 'C - Recommended',
+  //         link: '/style-guide/rules-recommended'
+  //       },
+  //       {
+  //         text: 'D - Use with Caution',
+  //         link: '/style-guide/rules-use-with-caution'
+  //       }
+  //     ]
+  //   }
+  // ]
 }
 
 // Placeholder of the i18n config for @vuejs-translations.
