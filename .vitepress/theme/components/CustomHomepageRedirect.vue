73058cd83b9f7125d008d535881f2cb6f1f66b47
liujunhong <liujunhong@vispractice.com>
2023-04-10 12:11:06 +0800

    验证修改默认导航的自定义插件

---
+++ .vitepress/theme/components/CustomHomepageRedirect.vue
@@ -0,0 +1,14 @@
+<template>
+    <div></div>
+  </template>
+  
+  <script>
+  export default {
+    mounted() {
+      if (this.$route.path === '/') {
+        this.$router.replace('/guide/user-guide/introduction');
+      }
+    },
+  };
+  </script>
+  
