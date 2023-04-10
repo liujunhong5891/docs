98a34e5c0509e2949ef894dbcedda11d6abe26de
liujunhong <liujunhong@vispractice.com>
2023-04-10 11:59:27 +0800

    验证自定义首页

---
+++ .vitepress/theme/components/Layout.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <!-- 页面主要内容 -->
+      <slot></slot>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    beforeRouteEnter(to, from, next) {
+      if (to.path === '/') {
+        next('/guide/user-guide/introduction');
+      } else {
+        next();
+      }
+    },
+  };
+  </script>
+  
