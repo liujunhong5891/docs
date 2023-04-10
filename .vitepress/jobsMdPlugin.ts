eff198f428277d7fd29cd317d2b712c4b6922573
liujunhong <liujunhong@vispractice.com>
2023-04-10 14:29:44 +0800

    删除框架中的示例文档

--- .vitepress/jobsMdPlugin.ts
+++ .vitepress/jobsMdPlugin.ts
@@ -1,30 +1,8 @@
 import MarkdownIt from 'markdown-it'
 
 const excludedPages = [
-  'guide/introduction.md',
-  'guide/quick-start.md',
-  'guide/essentials/computed.md',
-  'guide/essentials/conditional.md',
-  'guide/essentials/list.md',
-  'guide/essentials/event-handling.md',
-  'guide/essentials/forms.md',
-  'guide/components/registration.md',
-  'guide/components/props.md',
-  'guide/components/events.md',
-  'guide/components/slots.md',
-  'guide/built-ins/teleport.md',
-  'about/faq.md',
-  'about/team.md',
-  'about/releases.md',
-  'about/community-guide.md',
-  'about/coc.md',
-  'sponsor/index.md',
-  'translations/index.md',
-  // 自定义markdown begin
+    // 自定义markdown begin
   'index.md',
-  'guide/customer_test.md', 
-  'guide/CICD-quickStart.md',
-  'guide/docs-overview.md',
   'guide/default.md',
   'guide/user-guide/introduction.md',
   'guide/user-guide/quickstart-01.md',
